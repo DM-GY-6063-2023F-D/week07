2fca28f5abc3363f6bf45195a1ab6d2a1bb1a25a
thiagohersan <thiago.hersan+github@gmail.com>
2023-10-18 18:23:17 -0400

    create separate exaggerated and background removal sketches

---
+++ exaggerate-colors/sketch.js
@@ -0,0 +1,38 @@
+let mImg;
+
+function preload() {
+  mImg = loadImage("./abaporu.jpg")
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  mImg.resize(0, height);
+  noLoop();
+}
+
+function draw() {
+  background(220, 20, 120);
+
+  mImg.loadPixels();
+
+  for (let vi = 0; vi < mImg.pixels.length; vi += 4) {
+    let redVal = mImg.pixels[vi + 0];
+    let greenVal = mImg.pixels[vi + 1];
+    let blueVal = mImg.pixels[vi + 2];
+
+    let maxVal = max(redVal, greenVal, blueVal);
+
+    if (maxVal == redVal) {
+      mImg.pixels[vi + 0] = 255;
+    } else if (maxVal == greenVal) {
+      mImg.pixels[vi + 1] = 255;
+    } else {
+      mImg.pixels[vi + 2] = 255;
+    }
+  }
+
+  mImg.updatePixels();
+
+  image(mImg, (width - mImg.width)/2, 0);
+}
